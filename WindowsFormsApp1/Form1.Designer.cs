074eeaa34aaf6caefeb131b043e60ddcd6368f4b
Ding JunWei <1807216692@qq.com>
2020-02-23 21:42:31 +0800

    添加项目文件。

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,105 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.result = new System.Windows.Forms.Label();
+            this.firstNum = new System.Windows.Forms.TextBox();
+            this.secondNum = new System.Windows.Forms.TextBox();
+            this.operat = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(188, 255);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "运算";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // result
+            // 
+            this.result.AutoSize = true;
+            this.result.Location = new System.Drawing.Point(195, 344);
+            this.result.Name = "result";
+            this.result.Size = new System.Drawing.Size(67, 15);
+            this.result.TabIndex = 1;
+            this.result.Text = "运算结果";
+            this.result.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // firstNum
+            // 
+            this.firstNum.Location = new System.Drawing.Point(25, 171);
+            this.firstNum.Name = "firstNum";
+            this.firstNum.Size = new System.Drawing.Size(100, 25);
+            this.firstNum.TabIndex = 2;
+            // 
+            // secondNum
+            // 
+            this.secondNum.Location = new System.Drawing.Point(328, 171);
+            this.secondNum.Name = "secondNum";
+            this.secondNum.Size = new System.Drawing.Size(100, 25);
+            this.secondNum.TabIndex = 3;
+            // 
+            // operat
+            // 
+            this.operat.Location = new System.Drawing.Point(177, 171);
+            this.operat.Name = "operat";
+            this.operat.Size = new System.Drawing.Size(100, 25);
+            this.operat.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(476, 450);
+            this.Controls.Add(this.operat);
+            this.Controls.Add(this.secondNum);
+            this.Controls.Add(this.firstNum);
+            this.Controls.Add(this.result);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label result;
+        private System.Windows.Forms.TextBox firstNum;
+        private System.Windows.Forms.TextBox secondNum;
+        private System.Windows.Forms.TextBox operat;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double res = 0;
+            try
+            {
+                double firstnum = Double.Parse(firstNum.Text);
+                double secondnum = Double.Parse(secondNum.Text);
+                string opera = operat.Text;
+                switch (opera)
+                {
+                    case "+": res = firstnum + secondnum; break;
+                    case "-":res = firstnum - secondnum; break;
+                    case "*":res = firstnum * secondnum; break;
+                    case "/":res = firstnum / secondnum; break;
+                    default:result.Text = "错误"; return;
+                }
+                result.Text = $"运算结果为{res}";
+            }
+            catch(Exception x)
+            {
+                result.Text = "错误";
+            }
+        }
+    }
+}
