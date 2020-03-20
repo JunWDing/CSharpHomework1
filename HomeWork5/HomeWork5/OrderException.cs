ff64e8be03df7cad276a21b93c1fdb1b3dbc5864
JunWDing <50329454+JunWDing@users.noreply.github.com>
2020-03-20 10:42:20 +0800

    Add files via upload

---
+++ HomeWork5/HomeWork5/OrderException.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HomeWork5
+{
+    class OrderException:Exception
+    {
+        private string erroinfo;
+        public OrderException(string info) : base(info) { }
+    }
+}
